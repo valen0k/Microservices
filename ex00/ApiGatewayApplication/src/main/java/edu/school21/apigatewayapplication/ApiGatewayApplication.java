7a86af083135afa63fab58a4f68351c64ce9b0b5
valen0k <k.maksim-maksim@yandex.ru>
2022-04-01 00:45:59 +0300

    update ApiGatewayApplication

--- ex00/ApiGatewayApplication/src/main/java/edu/school21/apigatewayapplication/ApiGatewayApplication.java
+++ ex00/ApiGatewayApplication/src/main/java/edu/school21/apigatewayapplication/ApiGatewayApplication.java
@@ -2,14 +2,23 @@ package edu.school21.apigatewayapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class ApiGatewayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApiGatewayApplication.class, args);
-    }
+	@Bean
+	@LoadBalanced
+	public RestTemplate getTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
 
 }
